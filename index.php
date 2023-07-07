d9e1b3da698b97a8fe6fe29718cea2a7ef0a9604
Atif <arafique@contentenginellc.com>
2023-07-07 18:34:39 +0500

    Remove helper file which was created for expected external functions

--- index.php
+++ index.php
@@ -3,7 +3,6 @@ header('Content-Type: text/html; charset=utf-8');
 ini_set('max_execution_time', 900); 
 date_default_timezone_set("UTC");
 require_once(__DIR__."/lib/config.php");// db configuration class
-require_once(__DIR__."/lib/helper.php"); // helper functions
 
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
